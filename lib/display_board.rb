b42a073178ce191903eba802cfe43218cb51da12
PRATHAMESH PRABHUDESAI <prathamesh1993@gmail.com>
2018-12-31 04:00:52 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,5 +1,5 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
-def display_board:
+def display_board
   puts "   |   |   "
   puts "-----------"
   puts "   |   |   "
@@ -7,6 +7,4 @@ def display_board:
   puts "   |   |   "
 end
 
-def display_board
-  puts "A tic Tac Toe board"
-end
+
